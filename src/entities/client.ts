3191e6afcffa502e8e5725194b819c76eec864dd
Wassef911 <1.wassef911@gmail.com>
2021-12-03 23:44:37 +0100

    feat: user entity

--- src/entities/client.ts
+++ src/entities/client.ts
@@ -1,6 +1,25 @@
-import { Entity } from "typeorm";
+import { BaseEntity, Entity, Column } from "typeorm";
 
-
-export class Client {
-
+@Entity("client")
+export class Client extends BaseEntity {
+    @Column()
+    first_name: string;
+    @Column()
+    last_name: string;
+    @Column({
+        unique: true,
+    })
+    email: string;
+    @Column({ unique: true, length: 10 })
+    card_number: string
+    @Column({ type: "numeric" })
+    balance: number;
+    @Column({
+        default: true,
+    })
+    is_active: boolean;
+    @Column({ type: "simple-json" })
+    additional_info: {
+        age: number,
+    }
 }
