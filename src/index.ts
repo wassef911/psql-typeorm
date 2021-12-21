38620bc5c01629806510c3cc6c5cdad5d53a1f69
Wassef911 <1.wassef911@gmail.com>
2021-12-21 21:16:37 +0100

    fix: undo swagger

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,16 @@ import path from 'path';
 
 import express from 'express';
 import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express'
 
 import { establishConnection } from './config';
 import { Banker } from './entities/Banker';
 import { Client } from './entities/Client';
 import { Transaction } from './entities/Transaction';
 import routes from './routes';
-import * as swaggerDocument from './swagger.json'
 
 const ENTITIES = [Client, Banker, Transaction];
 const PORT = process.env.PORT || 3000;
 const app = express();
-const cssOptions = {
-    customCss: `
-    .swagger-ui .topbar { opacity:0 }`,
-    customSiteTitle: "Poopcode APIs"
-};
 
 try {
     establishConnection(ENTITIES).then(() => {
@@ -30,7 +23,6 @@ try {
         });
         app.use(morgan('combined', { stream: accessLogStream }));
         app.use('/', routes);
-        app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, cssOptions));
         const listener = app.listen(PORT, () => {
             const { address } = listener.address() as AddressInfo
             console.log("server running : http://%s:%s", address, PORT)
